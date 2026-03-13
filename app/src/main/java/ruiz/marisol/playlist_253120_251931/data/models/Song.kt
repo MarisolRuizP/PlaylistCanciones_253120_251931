537a3cff34c17c9723f7d349e690303b2f37aa64
Marisol <marisol.ruiz253120@potros.itson.edu.mx>
2026-03-12 18:45:14 -0700

    locura

---
+++ app/src/main/java/ruiz/marisol/playlist_253120_251931/data/models/Song.kt
@@ -0,0 +1,15 @@
+package ruiz.marisol.playlist_253120_251931.data.models
+
+import ruiz.marisol.playlist_253120_251931.R
+
+data class Song(
+    val id:Int,
+    val nombre:String,
+    val autor:String,
+    val duracion: Double,
+    val imagen: Int = 0
+)
+
+val sampleSongs = listOf(
+    Song(id = 1, nombre = "Your Favorite Toy", autor = "Foo Fighters", duracion = 2.55, imagen = R.drawable.Your_Favorite_Toy)
+)
---
+++ app/src/main/java/ruiz/marisol/playlist_253120_251931/ui/theme/screens/components/songCard.kt
@@ -0,0 +1,2 @@
+package ruiz.marisol.playlist_253120_251931.ui.theme.screens.components
+
